fe7cc68be5dd32314f34b45844a0ab4d1de1edfd
Ishmael Rogers <41645540+IshmaelRogers@users.noreply.github.com>
2018-11-15 04:21:59 -0500

    Update experimental_IKserver.py

--- experimental_IKserver.py
+++ experimental_IKserver.py
@@ -1,3 +1,33 @@
+#!/usr/bin/env python
+
+
+# import modules
+import rospy
+import tf
+from kuka_arm.srv import *
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from geometry_msgs.msg import Pose
+from mpmath import *
+from sympy import *
+from IK_helper import get_R_corr, get_WC, inv_pos, get_R0_3 inv_orient, TF_matrix
+
+
+# Define DH parameter symbols
+alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7') # twist angle
+a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7') # link length
+d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8') # link offset
+q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8') # joint angle
+
+# Modified DH parameters
+DH_table = {alpha0:      0, a0:      0, d1:   0.75, q1:        q1,
+            alpha1: -pi/2., a1:   0.35, d2:      0, q2: -pi/2.+q2,
+            alpha2:      0, a2:   1.25, d3:      0, q3:        q3,
+            alpha3: -pi/2., a3: -0.054, d4:    1.5, q4:        q4,
+            alpha4:  pi/2., a4:      0, d5:      0, q5:        q5,
+            alpha5: -pi/2., a5:      0, d6:      0, q6:        q6,
+            alpha6:      0, a6:      0, d7:  0.303, q7:         0}
+
+
 T0_1 = TF_Matrix(alpha0, a0, d1, q1).subs(DH_Table)
 T1_2 = TF_Matrix(alpha1, a1, d2, q2).subs(DH_Table)
 T2_3 = TF_Matrix(alpha2, a2, d3, q3).subs(DH_Table)
